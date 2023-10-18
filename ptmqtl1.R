cb3738c2fcb25251c40f6eef1b3d8d9d2d902c75
Chris Seybold <134991896+cseybold@users.noreply.github.com>
2023-10-17 21:52:06 -0700

    Update ptmqtl1.R

--- ptmqtl1.R
+++ ptmqtl1.R
@@ -5,66 +5,57 @@ set.seed(24601)
 library(tidyverse)
 
 
-p <- 0.5 # prob of SNP
-q <- 0.3 # prob of getting PTM w/o SNP
-z <- 0.2 # effect of SNP on PTM
-z <- min(z, 1-q)
-z <- max(z, -q) # ensure 0<q+z<1
-lambda_ <- 5 
 patient_count <- 20
 id_count <- 100
 
+lambda_ <- 5 
+p <- rep(rnorm(id_count, 0.4, 0.2), patient_count) # prob of SNP
+p <- pmax(pmin(1, p), 0)
+q <- rep(rnorm(id_count, 0.3, 0.15), patient_count) # prob of getting PTM w/o SNP
+q <- pmax(pmin(1, q), 0)
+z <- rep(sample(c(-0.2, 0, 0.2), size = id_count, replace = TRUE, prob = c(0.15,0.7,0.15)), patient_count) # prob of getting PTM w/o SNP
+z <- pmax(pmin(z, 1-q), -q) # ensure 0<q+z<1? -0.2<z<0 for some values?
 
-# n <- matrix(0, nrow = id_count, ncol = patient_count)
-# x <- matrix(0, nrow = id_count, ncol = patient_count)
-# y1 <- matrix(0, nrow = id_count, ncol = patient_count)
-# y2 <- matrix(0, nrow = id_count, ncol = patient_count)
-# y <- matrix(0, nrow = id_count, ncol = patient_count)
-# 
-# for (id_it in 1:id_count) {
-#   n[id_it,] <- rpois(patient_count, lambda = lambda_) # getting count of peptide
-#   x[id_it,] <- rbinom(patient_count, n[id_it,], p) # getting SNP count
-#   y1[id_it,] <- rbinom(patient_count, x[id_it,], q+z) # PTM w/ SNP
-#   y2[id_it,] <- rbinom(patient_count, n[id_it,]-x[id_it,], q) # PTM w/o SNP
-# }
-# y <- y1 + y2
 
 n = rpois(patient_count*id_count, lambda = lambda_)
+n = replace(n, n==0, 1)
 x = rbinom(patient_count*id_count, n, p)
 y1 = rbinom(patient_count*id_count, x, q+z)
 y2 = rbinom(patient_count*id_count, n-x, q)
+
 
 # n = matrix(n, nrow = id_count, ncol=patient_count)
 # x = matrix(x, nrow = id_count, ncol=patient_count)
 # y1 = matrix(y1, nrow = id_count, ncol=patient_count)
 # y2 = matrix(y2, nrow = id_count, ncol=patient_count)
-
+# 
 # n[1:5, 1:5]
 # x[1:5, 1:5]
 # y1[1:5, 1:5]
 # y2[1:5, 1:5]
 # 
-# n
-# x
-# y1
-# y2
-# y
+# n[19]
+# x[19]
+# y1[19]
+# y2[19]
+# y[19]
 
 # Convert the data to matrix form, this is the original form
 dat = data.frame(patient=rep(1:patient_count,id_count), 
                  snp_ptm_id=rep(1:id_count,each=patient_count), peptide=n,
                  snp=x,non_snp=n-x,snp_ptm=y1,non_snp_ptm=y2,effect_size=z)
-dat = as.tibble(dat)
+dat = as_tibble(dat)
+print(dat, n = 100)
 
 # Convert to a coarser form
 dat_coarse = dat
 dat_coarse$snp_type = dat_coarse$snp/dat_coarse$peptide
 dat_coarse$snp_type[dat_coarse$snp_type<0.33] = 0
 dat_coarse$snp_type[dat_coarse$snp_type>0.66] = 1
-dat_coarse$snp_type[dat_coarse$snp_type!=0 && dat_coarse$snp_type!=0] = 0.5
+dat_coarse$snp_type[(dat_coarse$snp_type != 0) & (dat_coarse$snp_type != 1)] = 0.5
 dat_coarse$ptm_count = dat_coarse$snp_ptm + dat_coarse$non_snp_ptm
-dat_coarse = dat_coarse %>% select(patient, snp_ptm_id, peptide, snp_type,
-                                   snp_ptm)
+dat_coarse = dat_coarse %>% select(patient, snp_ptm_id, peptide, snp_type, snp_ptm)
+print(dat_coarse, n = 100)
 ##################################################################
 # Task 1, compare the performance using dat and dat_coarse
 # Task 2, make the parameters sampled from distribution instead of using a fixed number
