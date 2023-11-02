93e5086539a2155b8633669d2dc6ec5a458c99b9
Chris Seybold <134991896+cseybold@users.noreply.github.com>
2023-11-02 16:52:21 -0700

    Update ptmqtl1.R

--- ptmqtl1.R
+++ ptmqtl1.R
@@ -4,22 +4,26 @@ set.seed(24601)
 library(tidyverse)
 
 
-patient_count <- 5
-id_count <- 10
+patient_count <- 50
+id_count <- 100
 
 lambda_ <- rep(sample(2:8, size = id_count, replace = TRUE), patient_count)
 p <- rep(rnorm(id_count, 0.4, 0.2), patient_count) # prob of SNP
 p <- pmax(pmin(1, p), 0)
-z <- rep(sample(c(-0.2, 0, 0.2), size = id_count, replace = TRUE, prob = c(0.15,0.7,0.15)), patient_count) # prob of getting PTM w/o SNP
+z <- numeric(patient_count*id_count)
+for (i in seq_len(patient_count)) {
+  z[((i-1)*id_count + 1):(i*id_count)] <- rep(sample(c(-0.2, 0, 0.2),
+    size = id_count, replace = TRUE, prob = c(0.15, 0.7, 0.15)), each = id_count) # prob of getting PTM w/o SNP
+}
 q <- rep(rnorm(id_count, 0.3, 0.15), patient_count) # prob of getting PTM w/o SNP
 q <- pmax(pmin(1, q), 0.2)
-
 
 n = rpois(patient_count*id_count, lambda = lambda_)
 n = replace(n, n==0, 1)
 x = numeric(patient_count*id_count)
 for (sampcnt in 1:(patient_count*id_count)) {
-  x[sampcnt] = min(n[sampcnt], sample(c(0, 0.5, 1), size = 1, replace = TRUE, prob = c((1-p[sampcnt])^2, p[sampcnt]^2, max(1-((1-p[sampcnt])^2 + p[sampcnt]^2), 0))))
+  x[sampcnt] = min(n[sampcnt], sample(c(0, 0.5, 1), size = 1, replace = TRUE,
+    prob = c((1-p[sampcnt])^2, p[sampcnt]^2, max(1-((1-p[sampcnt])^2 + p[sampcnt]^2), 0))))
 }
 x[x == 1] <- n[x == 1]
 x[x == 0.5] <- rbinom(length(x[x == 0.5]), n[x == 0.5], 0.5)
@@ -59,24 +63,44 @@ dat_coarse$snp_type[dat_coarse$snp_type>0.66] = 1
 dat_coarse$snp_type[(dat_coarse$snp_type != 0) & (dat_coarse$snp_type != 1)] = 0.5
 dat_coarse$effect_size = dat_coarse$effect_size
 dat_coarse = dat_coarse %>% select(patient, snp_ptm_id, peptide, snp_type, snp_ptm, effect_size)
+
+dat_coarse$snp_ptm <- dat_coarse$snp_ptm / dat_coarse$peptide
+
 print(dat_coarse, n = 100)
 
-
+#coarse data linear regression plot
 coarse_lm_pval <- numeric(id_count)
 for (idc in 1:id_count) {
-  coarse_lm <- lm(snp_ptm[(1+(patient_count*(idc-1))):(patient_count*idc)] ~ snp_type[(1+(patient_count*(idc-1))):(patient_count*idc)], data = dat_coarse)
+  coarse_lm <- lm(snp_ptm[(1+(patient_count*(idc-1))):(patient_count*idc)]
+    ~ snp_type[(1+(patient_count*(idc-1))):(patient_count*idc)], data = dat_coarse)
   coarse_lm_pval[idc] <- summary(coarse_lm)$coefficients[2,4]
 }
 sig_col <- ifelse(coarse_lm_pval < 0.05, "red", "black")
-plot(seq_len(id_count), coarse_lm_pval, main = "Significance of snp-ptm correlation", xlab = "snp-ptm id", ylab = "p-value", pch = 20, col = sig_col)
+plot(seq_len(id_count), coarse_lm_pval, main = "Significance of snp-ptm correlation (new)",
+     xlab = "snp-ptm id", ylab = "p-value", pch = 20, col = sig_col)
 abline(h = 0.05, col = "red4")
 
+
+#old data linear regression plot
 old_lm_pval <- numeric(id_count)
 for (idc in 1:id_count) {
-  old_lm <- lm(ptm[sum(n[0:(patient_count*(idc-1))]):sum(n[0:(patient_count*idc)])] ~ snp[sum(n[0:(patient_count*(idc-1))]):sum(n[0:(patient_count*idc)])], data = new_dat)
+  old_lm <- lm(ptm[sum(n[0:(patient_count*(idc-1))]):sum(n[0:(patient_count*idc)])]
+    ~ snp[sum(n[0:(patient_count*(idc-1))]):sum(n[0:(patient_count*idc)])], data = new_dat)
   old_lm_pval[idc] <- summary(old_lm)$coefficients[2,4]
 }
 
 sig_col_2 <- ifelse(old_lm_pval < 0.05, "blue", "black")
-plot(seq_len(id_count), old_lm_pval, main = "Significance of snp-ptm correlation", xlab = "snp-ptm id", ylab = "p-value", pch = 20, col = sig_col_2)
+plot(seq_len(id_count), old_lm_pval, main = "Significance of snp-ptm correlation (old)",
+     xlab = "snp-ptm id", ylab = "p-value", pch = 20, col = sig_col_2)
 abline(h = 0.05, col = "blue4")
+
+
+#coarse data analysis (id vs bvalue and snp-type vs normalized ptm count)
+coarse_lm_b <- numeric(id_count)
+for (idc in 1:id_count) {
+  coarse_lm <- lm(snp_ptm[(1+(patient_count*(idc-1))):(patient_count*idc)]
+                  ~ snp_type[(1+(patient_count*(idc-1))):(patient_count*idc)], data = dat_coarse)
+  coarse_lm_b[idc] <- coef(coarse_lm)[2]
+}
+plot(dat_coarse$snp_type, dat_coarse$snp_ptm)
+plot(1:100, coarse_lm_b, xlab = "snp-ptm_id", ylab = "B-value")
